1afcaf217c739faa653e36dfefa34473d6e53483
Anderson Silva <andersonsoufeliz27@gmail.com>
2023-12-18 20:15:24 -0300

    manipulando perguntas através do JavaScript parte 2

--- js/script.js
+++ js/script.js
@@ -14,8 +14,23 @@ links.forEach(ativarLink);
 const parametros = new URLSearchParams(location.search);
 function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
- if (elemento){
-  elemento.checked = true;
-}
+  if (elemento) {
+    elemento.checked = true;
+  }
 }
 parametros.forEach(ativarProduto);
+
+/**Perguntas frequentes */
+
+const perguntas = document.querySelectorAll(".perguntas button");
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  console.log(pergunta);
+}
+
+function eventosPerguntas(pergunta) {
+  addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
