0024bbcfe87a6db226e3ee2ed38ec2be140b9478
Anderson Silva <andersonsoufeliz27@gmail.com>
2023-12-19 11:01:35 -0300

    manipulando perguntas através do JavaScript parte 3

--- js/script.js
+++ js/script.js
@@ -25,12 +25,17 @@ parametros.forEach(ativarProduto);
 const perguntas = document.querySelectorAll(".perguntas button");
 function ativarPergunta(event) {
   const pergunta = event.currentTarget;
-  const controls = pergunta.getAttribute('aria-controls');
-  console.log(pergunta);
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls)
+  resposta.classList.toggle("ativa")
+  const ativa = resposta.classList.contains("ativa");
+  // console.log(ativa);
+  pergunta.setAttribute("aria-expanded", ativa);
+  // console.log(resposta);
 }
 
 function eventosPerguntas(pergunta) {
-  addEventListener("click", ativarPergunta);
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(eventosPerguntas);
